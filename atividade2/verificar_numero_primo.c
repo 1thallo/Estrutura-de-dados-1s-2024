e3065111914c8a31839d5ae6fb289c2dcce753d6
1thallo <ithalloflu@gmail.com>
2024-03-26 21:22:41 -0300

    atividade2

---
+++ atividade2/verificar_numero_primo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+  int main(int argc, char *argv[]){
+    if (argc != 2){
+        printf("\nInsira um numero inteiro!");
+        exit(0);
+    }
+      if (atoi(argv[1])<2){
+        printf("//false");
+        return -1;
+      }
+        for (int i=2; i<atoi(argv[1]); i++){
+          if (atoi(argv[1]) % i == 0){
+            printf("//false");
+            return -1;
+        }
+    }
+    printf("//true");
+    exit(0);
+}
